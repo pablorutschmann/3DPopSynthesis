4a8bc6d6049427148f779be1f6f1fd44e7aebe0f
pablorutschmann <paphiru@gmail.com>
2021-09-28 08:24:09 +0200

    clean up

---
+++ disk_preprocessing.py
@@ -0,0 +1,11 @@
+import os, sys
+import numpy as np
+from scipy.optimize import curve_fit
+import astropy.units as u
+from os import listdir
+from os.path import isfile, join
+from math import floor
+from tqdm import tqdm
+import itertools
+
+
