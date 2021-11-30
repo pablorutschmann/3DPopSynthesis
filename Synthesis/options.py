5b063fe16a343c930ff924b25fa360be0d8b1b12
pablorutschmann <paphiru@gmail.com>
2021-11-30 12:44:37 +0100

    started sythnesis framwork

--- Synthesis/options.py
+++ Synthesis/options.py
@@ -1,5 +1,35 @@
-import os
+def write(path):
+
+    options = {}
+    with open("options_template.txt") as f:
+        for line in f:
+            (key, val) = line.split()
+            options[key] = val
+    print(options)
+
+    # Number of Embryos
+    NEmbryos = np.random.uniform(low = 7, high = 14)
+    options["NEmbryos"] = NEmbryos
+
+    # Number of Planetesiamls
+    NPlanetesimals = 100
+    options["NPlanetesimals"] = NPlanetesimals
+
+    # Initial Mass of Embryos
+    EmbryoInitMass =
 
 
-cmd = "echo 'hello world'"
-os.system(cmd)
+
+
+    with open('options.txt', 'w') as f:
+        f.write("{0}\t{1}\n".format("NEmbryos", NEmbryos)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    write('here')
--- Synthesis/setup.py
+++ Synthesis/setup.py
@@ -1,1 +1,43 @@
+import sys
+import os.path as path
+from os import getcwd
+from os import makedirs
+
+def setup(NAME, N_SIMS):
+    runname = NAME
+
+    N_sims = int(N_SIMS)
+
+    CWD = getcwd()
+    PARENT = path.dirname(CWD)
+
+    RUN = path.join(PARENT, 'SynthesisRuns', runname)
+
+    print(RUN)
+
+    def dir_structure(index):
+        SYSTEM = path.join(RUN,'system_' + str(index).zfill(4))
+
+        INPUT = path.join(SYSTEM, 'inputs')
+        OUTPUT = path.join(SYSTEM, 'outputs')
+        makedirs(INPUT)
+        makedirs(OUTPUT)
+
+    for i in range(N_sims):
+        dir_structure(i)
+        #Create DiskFile
+
+        #Create Options File
+
+    return RUN
+
+if __name__ == "__main__":
+    Name = sys.argv[1]
+    N = sys.argv[2]
+    Path = setup(Name, N)
+
+
+
+
+
 
